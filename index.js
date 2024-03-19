2729c0e90d86084d8bea4532c547e668afe5e699
student <kmac.kimaru93@gmail.com>
2024-03-19 22:06:04 +0300

    adding function cont

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+//default arguments are overridden by the main arguments
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+console.log(mondayWork("work from home."));
+
+//function-level scope
+function wrapAdjective(character = "*") {
+  return function (inner = "special") {
+    return `You are ${character}${inner}${character}!`;
+  };
+  result(`You are a ${character}hard worker${character}!`);
+}
+
+const specialFunction = wrapAdjective("||");
+specialFunction("a hard worker");
